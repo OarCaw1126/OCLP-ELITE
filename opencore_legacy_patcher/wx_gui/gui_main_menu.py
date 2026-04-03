56df9fb852c068bac47a7aea2ca2760793eb5f61
TORXED <tiendacreator@gmail.com>
2026-04-02 19:12:38 -0700

    Rename To OCLP ELITE Copied And Modified To Ybronst

--- opencore_legacy_patcher/wx_gui/gui_main_menu.py
+++ opencore_legacy_patcher/wx_gui/gui_main_menu.py
@@ -77,7 +77,7 @@ class MainFrame(wx.Frame):
         logo.Centre(wx.HORIZONTAL)
 
         # Title label: OpenCore Legacy Patcher v{X.Y.Z}
-        title_label = wx.StaticText(self, label=f"OpenCore Legacy Patcher", pos=(-1, 128))
+        title_label = wx.StaticText(self, label=f"OCLP ELITE", pos=(-1, 128))
         title_label.SetFont(gui_support.font_factory(25, wx.FONTWEIGHT_BOLD))
         title_label.Centre(wx.HORIZONTAL)
 
@@ -228,7 +228,7 @@ class MainFrame(wx.Frame):
             self.constants.has_checked_updates = True
             pop_up = wx.MessageDialog(
                 self,
-                f"OpenCore Legacy Patcher has been updated to the latest version: {self.constants.patcher_version}\n\nWould you like to update OpenCore and your root volume patches?",
+                f"OCLP ELITE has been updated to the latest version: {self.constants.patcher_version}\n\nWould you like to update OpenCore and your root volume patches?",
                 "Update successful!",
                 style=wx.YES_NO | wx.YES_DEFAULT | wx.ICON_INFORMATION
             )
@@ -341,8 +341,8 @@ Please check the Github page for more information about this release."""
         panel = wx.Panel(frame)
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.AddSpacer(10)
-        self.title_text = wx.StaticText(panel, label="A new version of OpenCore Legacy Patcher is available!")
-        self.description = wx.StaticText(panel, label=f"OpenCore Legacy Patcher {oclp_version} is now available - You have {self.constants.patcher_version}{' (Nightly)' if not self.constants.commit_info[0].startswith('refs/tags') else ''}. Would you like to update?")
+        self.title_text = wx.StaticText(panel, label="A new version of OCLP ELITE is available!")
+        self.description = wx.StaticText(panel, label=f"OCLP ELITE {oclp_version} is now available - You have {self.constants.patcher_version}{' (Nightly)' if not self.constants.commit_info[0].startswith('refs/tags') else ''}. Would you like to update?")
         self.title_text.SetFont(gui_support.font_factory(19, wx.FONTWEIGHT_BOLD))
         self.description.SetFont(gui_support.font_factory(13, wx.FONTWEIGHT_NORMAL))
         self.web_view = wx.html2.WebView.New(panel, style=wx.BORDER_SUNKEN)
