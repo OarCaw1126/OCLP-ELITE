cf557514bcb77aeaeb4f4169d95e2dd5a97ac14f
TORXED <tiendacreator@gmail.com>
2026-05-05 20:20:21 -0500

    Rename Opencore To OCLP elite part 17

--- oclp_elite/constants.py
+++ oclp_elite/constants.py
@@ -748,7 +748,7 @@ class Constants:
 
     @property
     def oclp_helper_path(self):
-        return self.payload_path / Path("Tools/OpenCore-Patcher.app/Contents/MacOS/OpenCore-Patcher")
+        return self.payload_path / Path("Tools/OCLP-ELITE.app/Contents/MacOS/OpenCore-Patcher")
 
     @property
     def rsrrepair_userspace_path(self):
