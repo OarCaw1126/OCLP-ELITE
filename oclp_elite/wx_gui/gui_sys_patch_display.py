83bb1e9b960dba1cea9a9e7d58b0361ab9c15a1a
TORXED <tiendacreator@gmail.com>
2026-05-05 20:47:32 -0500

    Update plist path for OpenCore Legacy Patcher

--- oclp_elite/wx_gui/gui_sys_patch_display.py
+++ oclp_elite/wx_gui/gui_sys_patch_display.py
@@ -308,7 +308,7 @@ class SysPatchDisplayFrame(wx.Frame):
             # If commits are different, assume patches are as well
             return True
 
-        oclp_plist = "/System/Library/CoreServices/OpenCore-Legacy-Patcher.plist"
+        oclp_plist = "/System/Library/CoreServices/OCLP-ELITE.plist"
         if not Path(oclp_plist).exists():
             # If it doesn't exist, no patches were ever installed
             # ie. all patches applicable
