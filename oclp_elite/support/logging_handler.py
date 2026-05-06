e09301fbf285220d6b287bad008546726f909d41
TORXED <tiendacreator@gmail.com>
2026-05-05 20:30:03 -0500

    Rename logging references from OpenCore to OCLP-ELITE

--- oclp_elite/support/logging_handler.py
+++ oclp_elite/support/logging_handler.py
@@ -47,7 +47,7 @@ class InitializeLoggingSupport:
 
         log_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
 
-        self.log_filename: str  = f"OpenCore-Patcher_{self.constants.patcher_version}_{log_time}.log"
+        self.log_filename: str  = f"OCLP-ELITE_{self.constants.patcher_version}_{log_time}.log"
         self.log_filepath: Path = None
 
         self.original_excepthook:        sys       = sys.excepthook
@@ -100,7 +100,7 @@ class InitializeLoggingSupport:
         logs = []
 
         for path in paths:
-            for file in path.glob("OpenCore-Patcher*"):
+            for file in path.glob("OCLP-ELITE*"):
                 if not file.is_file():
                     continue
 
@@ -166,7 +166,7 @@ class InitializeLoggingSupport:
         Start logging, used as easily identifiable start point in logs
         """
 
-        str_msg = f"# OpenCore Legacy Patcher ({self.constants.patcher_version}) #"
+        str_msg = f"# OCLP-ELITE ({self.constants.patcher_version}) #"
         str_len = len(str_msg)
 
         logging.info('#' * str_len)
@@ -202,7 +202,7 @@ class InitializeLoggingSupport:
             if self.constants.cli_mode is True:
                 return
 
-            error_msg = f"OpenCore Legacy Patcher encountered the following internal error:\n\n"
+            error_msg = f"OCLP-ELITE encountered the following internal error:\n\n"
             error_msg += f"{type.__name__}: {value}"
             if tb:
                 error_msg += f"\n\n{traceback.extract_tb(tb)[-1]}"
@@ -211,7 +211,7 @@ class InitializeLoggingSupport:
 
             # Ask user if they want to send crash report
             try:
-                result = applescript.AppleScript(f'display dialog "{error_msg}" with title "OpenCore Legacy Patcher ({self.constants.patcher_version})" buttons {{"Yes", "No"}} default button "Yes" with icon caution').run()
+                result = applescript.AppleScript(f'display dialog "{error_msg}" with title "OCLP-ELITE ({self.constants.patcher_version})" buttons {{"Yes", "No"}} default button "Yes" with icon caution').run()
             except Exception as e:
                 logging.error(f"Failed to display crash report dialog: {e}")
                 return
