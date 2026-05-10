9549cfe791ef417269f693a6f3acf8b966a9ceb5
TORXED <tiendacreator@gmail.com>
2026-05-10 12:38:18 -0500

    Bump Version to 3.2.4 And Make Some Modifications

--- oclp_elite/constants.py
+++ oclp_elite/constants.py
@@ -13,18 +13,18 @@ from .detections import device_probe
 class Constants:
     def __init__(self) -> None:
         # Patcher Versioning
-        self.patcher_version:                 str = "3.2.3"  # OpenCore-Legacy-Patcher
+        self.patcher_version:                 str = "3.2.4"  # OpenCore-Legacy-Patcher
         self.patcher_support_pkg_version:     str = "2.0.0"  # PatcherSupportPkg
         self.copyright_date:                  str = "Copyright © 2020-2026 Dortania And Torxed"
-        self.patcher_name:                    str = "OpenCore Legacy Patcher"
+        self.patcher_name:                    str = "OCLP-ELITE"
 
         # URLs
         self.url_patcher_support_pkg:         str = "https://github.com/YBronst/PatcherSupportPkg/releases/tag/2.0.0/Universal-Binaries.dmg"
         self.discord_link:                    str = "https://discord.gg/rqdPgH8xSN"
         self.guide_link:                      str = "https://dortania.github.io/OpenCore-Legacy-Patcher/"
-        self.repo_link:                       str = "https://github.com/dortania/OpenCore-Legacy-Patcher"
+        self.repo_link:                       str = "https://github.com/OarCaw1126/OCLP-ELITE"
         self.installer_pkg_url:               str = f"{self.repo_link}/releases/download/{self.patcher_version}/AutoPkg-Assets.pkg"
-        self.installer_pkg_url_nightly:       str = "http://nightly.link/dortania/OpenCore-Legacy-Patcher/workflows/build-app-wxpython/main/AutoPkg-Assets.pkg.zip"
+        self.installer_pkg_url_nightly:       str = "http://nightly.link/OarCaw1126/OCLP-ELITE/workflows/build-app-wxpython/tahoe-patchset/AutoPkg-Assets.pkg.zip"
 
         # OpenCore Versioning
         # https://github.com/acidanthera/OpenCorePkg
@@ -249,6 +249,7 @@ class Constants:
             os_data.os_data.ventura,
             os_data.os_data.sonoma,
             os_data.os_data.sequoia,
+            os_data.os_data.tahoe,
         ]
 
     @property
